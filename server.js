8cb0ab41de76bf6cc4f8c810cb388b362105a4f8
rafiq-mohammed <64529864+rafiq-mohammed@users.noreply.github.com>
2020-05-04 14:21:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var path = require('path');
 const app = express();
 
 //Set the base path to the angular-test dist folder
-app.use(express.static(path.join(__dirname, 'dist/')));
+app.use(express.static(path.join(__dirname, '/')));
 
 //Any routes will be redirected to the angular app
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 //Starting server on port 8081
